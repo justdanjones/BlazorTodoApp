b6bf775c31689ae99a91621cedef6ed536be49ab
Daniel Jones <danieljones@fife.ac.uk>
2019-10-04 16:03:16 +0100

    Initial commit

---
+++ Shared/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApp.Shared
+{
+    public class Todo
+    {
+        public Guid ID { get; set; }
+        public string Text { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
